30c899efb2dd2dd70240fb9769fb7f6af5d75910
Satwik Garg <satwikgarg@Satwiks-MacBook-Air.local>
2024-05-30 19:12:22 +0530

    Completed project

--- game.js
+++ game.js
@@ -2,14 +2,55 @@ var start = false;
 var patternList = [];
 var userEnteredPattern = [];
 var coloursList = ["#blue", "#red", "#green", "#yellow"];
+var level = 0;
 
-function generatePattern(){
-    var randomNumber = Math.floor(Math.random()*4);
-    patternList.push(coloursList[randomNumber]);
+$(document).keypress(function(){
+    if (!start){
+        start = true;
+        $("#level-title").text("Press A key to start.");
+        generatePattern();
+    }
+});
+
+$(".btn").click(function(){
+        handler(this.id);
+});
+
+function handler(colour){
+    userEnteredPattern.push("#"+colour);
+    addEffects("#"+colour, "pressed");
+    checkPattern(userEnteredPattern.length-1);
+}    
+
+function checkPattern(lastUserEnteredIndex){
+    if (userEnteredPattern[lastUserEnteredIndex]===patternList[lastUserEnteredIndex]){
+        console.log("success");
+        if (userEnteredPattern.length===patternList.length){
+            setTimeout(function(){
+                generatePattern();
+            }, 1000);
+        }
+    } else {
+        endGame();
+    }
 }
 
-function showPattern(){
-    addEffects(patternList[patternList.length-1], effect);
+function endGame(){
+        start = false;
+        level = 0;
+        patternList = [];
+        userEnteredPattern = [];
+        $("h1").text("Game Over, press any key to restart.");
+        addEffects("body", "game-over");
+}
+
+function generatePattern(){
+    userEnteredPattern = [];
+    level++;
+    $("h1").text("Level "+level);
+    var randomNumber = Math.floor(Math.random()*4);
+    patternList.push(coloursList[randomNumber]);
+    addEffects(patternList[patternList.length-1], "pressed");
 }
 
 function addEffects(id, effect){
@@ -17,12 +58,11 @@ function addEffects(id, effect){
     setTimeout(function(){
         $(id).removeClass(effect);
     }, 100);
-    if (effect==='pressed'){
+    if (effect==="pressed"){
         var audio = new Audio("sounds/"+id.slice(1)+".mp3");
         audio.play();
     } else if (effect==='game-over') {
         var audio = new Audio("sounds/wrong.mp3");
         audio.play();
     }
-    
 }
